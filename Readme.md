57c640cc616dc4421f9655287bf075deb190fc0d
Đoàn Bá Huy <fxdoanhuy@gmail.com>
2021-04-08 23:36:28 +0700

    Init project

---
+++ Readme.md
@@ -0,0 +1,8 @@
+### My - VUE2 - Boilerplate
+Build the vue2 boilerplate from scratch with 
+```
+- webpack 
+- babel
+```
+
+*(Ref [FreeCodeCamp](https://www.freecodecamp.org/news/how-to-create-a-vue-js-app-using-single-file-components-without-the-cli-7e73e5b8244f/))*
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+     <input
+      v-model="message"
+      type="text"/>
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
+}
+</style>
